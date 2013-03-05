757cbbe389fda96fc1564a4b8872e38df3732b96
Alexander Lehmann <afwlehmann@googlemail.com>
2013-03-05 12:17:34 +0100

    More license foo.

--- contents/code/main.py
+++ contents/code/main.py
@@ -1,8 +1,20 @@
 #
 # main.py
-# copyright (c) by Jayme <tunxnet@gmail.com>
-#                  Alexander Lehmann <afwlehmann@googlemail.com>
+# copyright (c) 2013 by Jayme <tunxnet@gmail.com>
+#                       Alexander Lehmann <afwlehmann@googlemail.com>
 #
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os, sqlite3, json
 from shutil import copy2
